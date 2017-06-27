f675d9fa1a08ed838dcf1f4494c1ef80da43ac20
brucecham <brucecham@qq.com>
2017-06-27 14:40:23 +0800

    part4 add content

--- code/part-4/app.js
+++ code/part-4/app.js
@@ -1,7 +1,12 @@
 'use strict';
+import util from './utils/index';
+
 let handler = {
     onLaunch () {
         this.getDevideInfo();
+        util.getStorageData('visited', (data)=> {
+            this.globalData.visitedArticles = data; 
+        });
     },
     alert (title = '提示', content = '好像哪里出了小问题~请再试一次~') {
         wx.showModal({
